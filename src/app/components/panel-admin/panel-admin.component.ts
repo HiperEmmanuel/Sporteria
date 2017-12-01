ba8e8735344e88f1bf6c2950cba5e627e44945bd
HiperEmmanuel <hiperemmanuel@gmail.com>
2017-11-30 19:30:11 -0700

    Validaciones

--- src/app/components/panel-admin/panel-admin.component.ts
+++ src/app/components/panel-admin/panel-admin.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-panel-admin',
   templateUrl: './panel-admin.component.html',
   styles: []
 })
+
 export class PanelAdminComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() {
+   }
+  
   ngOnInit() {
   }
 
